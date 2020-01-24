040d4ebd9694fd95f945d8408db86bfe2dc2c733
Salimov Bunyod <bunyod545@mail.ru>
2020-01-24 07:59:48 +0500

    SB.Report project begin

---
+++ src/SB.Report/Logics/ExcelTemplate/ExcelTemplateReport.cs
@@ -0,0 +1,30 @@
+﻿using OfficeOpenXml;
+using SB.Report.Logics.ExcelTemplate.Helpers;
+using System.IO;
+
+namespace SB.Report.Logics.ExcelTemplate
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class ExcelTemplateReport
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public abstract ExcelPackage Generate();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public virtual ExcelPackage GetTemplate()
+        {
+            var templateReportStream = TemplateReportHelper.GetTemplateReportStream(this);
+            if (templateReportStream == null)
+                throw new FileNotFoundException("Excel template report not found!");
+
+            return new ExcelPackage(templateReportStream);
+        }
+    }
+}
