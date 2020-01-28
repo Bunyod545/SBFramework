38d953aee9de0a71f8c36f73fc7eca02b6b8fed5
Salimov Bunyod <bunyod545@mail.ru>
2020-01-28 07:15:09 +0500

    SB.Report Excel row and column settings copy logic added

--- src/SB.Report/Logics/ExcelTemplate/Extensions/ExcelNamedRangeExtensions.cs
+++ src/SB.Report/Logics/ExcelTemplate/Extensions/ExcelNamedRangeExtensions.cs
@@ -14,7 +14,44 @@ namespace SB.Report.Logics.ExcelTemplate.Extensions
         /// <param name="range"></param>
         public static void CopySettings(this ExcelNamedRange namedRange, ExcelRange range)
         {
+            namedRange.CopyColumnsSettings(range);
+            namedRange.CopyRowsSettings(range);
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="namedRange"></param>
+        /// <param name="range"></param>
+        private static void CopyColumnsSettings(this ExcelNamedRange namedRange, ExcelRange range)
+        {
+            for (var i = 0; i < namedRange.Columns; i++)
+            {
+                var tempColumnIndex = namedRange.Start.Column + i;
+                var tempColumn = namedRange.Worksheet.Column(tempColumnIndex);
 
+                var columnIndex = range.Start.Column + i;
+                var column = range.Worksheet.Column(columnIndex);
+                column.CopyFromTemplate(tempColumn);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="namedRange"></param>
+        /// <param name="range"></param>
+        private static void CopyRowsSettings(this ExcelNamedRange namedRange, ExcelRange range)
+        {
+            for (var i = 0; i < namedRange.Rows; i++)
+            {
+                var tempRowIndex = namedRange.Start.Row + i;
+                var tempRow = namedRange.Worksheet.Row(tempRowIndex);
+
+                var rowIndex = range.Start.Column + i;
+                var row = range.Worksheet.Row(rowIndex);
+                row.CopyFromTemplate(tempRow);
+            }
         }
     }
 }
