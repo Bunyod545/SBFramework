7a8725ea71bab12d3b89f16a2a5ecbd8e27eb913
HP <HP@DESKTOP-GDLLM62>
2021-05-16 22:28:44 +0500

    deploy corrected

--- src/CodeMigration/SB.CodeMigration.AspNetCore/ApplicationBuilderExtensions.cs
+++ src/CodeMigration/SB.CodeMigration.AspNetCore/ApplicationBuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 
 namespace SB.CodeMigration.AspNetCore
@@ -15,14 +16,18 @@ namespace SB.CodeMigration.AspNetCore
         /// <param name="actualVersion"></param>
         public static void UseCodeMigration(this IApplicationBuilder app)
         {
+            var scopeFactory = (IServiceScopeFactory)app.ApplicationServices.GetService(typeof(IServiceScopeFactory));
+            var scope = scopeFactory.CreateScope();
+
             var defaultOptions = new CodeMigrationDefaultOptions();
-            defaultOptions.Activator = new CodeMigrationDIActivator(app.ApplicationServices);
+            defaultOptions.Activator = new CodeMigrationDIActivator(scope.ServiceProvider);
 
             var versionManager = (ICodeMigrationVersionManager)app.ApplicationServices.GetService(typeof(ICodeMigrationVersionManager));
             if (versionManager == null)
                 throw new Exception("ICodeMigrationVersion manager not registered to DI");
 
             CodeMigrationManager.Migrate(versionManager, defaultOptions);
+            scope.Dispose();
         }
     }
 }
---
+++ src/CodeMigration/SB.CodeMigration.AspNetCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SB.CodeMigration.AspNetCore
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddCodeMigration<TMigrationVersionManager>(this IServiceCollection services) where TMigrationVersionManager : class, ICodeMigrationVersionManager
+        {
+            services.AddScoped<ICodeMigrationVersionManager, TMigrationVersionManager>();
+        }
+    }
+}
